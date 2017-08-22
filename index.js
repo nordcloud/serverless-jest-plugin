31e968657d93f8ef61120a57e53a70b83ed669fe
Eric Chan <eric.chan@intellihr.com.au>
2017-08-22 15:23:12 +1000

    Exit the process with code 1 when the jest is failed

--- index.js
+++ index.js
@@ -91,7 +91,15 @@ class ServerlessJestPlugin {
           .then(() => createTest(this.serverless, this.options)),
       'invoke:test:test': () =>
         BbPromise.bind(this)
-          .then(() => runTests(this.serverless, this.options, this.config)),
+          .then(() => runTests(this.serverless, this.options, this.config))
+          .catch((err) => {
+            if (err.success === false) {
+              // This is a successful run but with failed tests
+              process.exit(1);
+            }
+            // Not sure what this is
+            throw err;
+          }),
       'create:function:create': () =>
         BbPromise.bind(this)
           .then(() => createFunction(this.serverless, this.options))
