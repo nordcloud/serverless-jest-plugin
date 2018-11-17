b32eec5e5af040be3e0b5faafad765bac309eec3
Doug McCall <dhm116@gmail.com>
2018-11-17 11:48:30 -0500

    Improve config coverage, DRY things up

--- __tests__/config.test.js
+++ __tests__/config.test.js
@@ -15,6 +15,24 @@ jest.mock('serverless/lib/classes/CLI', () =>
   }),
 );
 
+const writeServerlessConfig = (jestOptions) => {
+  const serverlessConfig = `
+  service: my-service
+  provider:
+    name: aws
+    runtime: nodejs8.10
+  plugins:
+    - serverless-jest-plugin
+  custom:
+    jest:
+      ${jestOptions}
+  functions:
+    hello:
+      handler: handler.hello
+  `;
+  fs.writeFileSync('serverless.yml', serverlessConfig);
+};
+
 describe('jest configuration', () => {
   beforeAll(() => {
     process.env.PLUGIN_TEST_DIR = path.join(__dirname);
@@ -92,24 +110,10 @@ describe('jest configuration', () => {
   });
 
   it('passes the serverless jest config through to jest', () => {
-    fs.writeFileSync(
-      'serverless.yml',
-      `
-      service: my-service
-      provider:
-        name: aws
-        runtime: nodejs8.10
-      plugins:
-        - serverless-jest-plugin
-      custom:
-        jest:
-          verbose: false
-          collectCoverage: true
-          useStderr: true
-      functions:
-        hello:
-          handler: handler.hello
-      `,
+    writeServerlessConfig(
+      `verbose: false
+      collectCoverage: true
+      useStderr: true`,
     );
 
     expect(fs.existsSync('coverage')).toBeFalsy();
@@ -143,24 +147,10 @@ describe('jest configuration', () => {
   }, 10000);
 
   it('modifies the serverless jest config and verifies the changes', () => {
-    fs.writeFileSync(
-      'serverless.yml',
-      `
-      service: my-service
-      provider:
-        name: aws
-        runtime: nodejs8.10
-      plugins:
-        - serverless-jest-plugin
-      custom:
-        jest:
-          verbose: true
-          collectCoverage: false
-          useStderr: true
-      functions:
-        hello:
-          handler: handler.hello
-      `,
+    writeServerlessConfig(
+      `verbose: true
+      collectCoverage: false
+      useStderr: true`,
     );
 
     expect(fs.existsSync('coverage')).toBeFalsy();
@@ -197,23 +187,9 @@ describe('jest configuration', () => {
   it('allows testRegex to be changed', () => {
     const regex = '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$';
 
-    fs.writeFileSync(
-      'serverless.yml',
-      `
-      service: my-service
-      provider:
-        name: aws
-        runtime: nodejs8.10
-      plugins:
-        - serverless-jest-plugin
-      custom:
-        jest:
-          useStderr: true
-          testRegex: ${regex}
-      functions:
-        hello:
-          handler: handler.hello
-      `,
+    writeServerlessConfig(
+      `useStderr: true
+      testRegex: ${regex}`,
     );
 
     const serverless = new Serverless({ interactive: false });
@@ -241,23 +217,9 @@ describe('jest configuration', () => {
   }, 10000);
 
   it('can test all test files, not just function-related ones', () => {
-    fs.writeFileSync(
-      'serverless.yml',
-      `
-      service: my-service
-      provider:
-        name: aws
-        runtime: nodejs8.10
-      plugins:
-        - serverless-jest-plugin
-      custom:
-        jest:
-          verbose: true
-          useStderr: true
-      functions:
-        hello:
-          handler: handler.hello
-      `,
+    writeServerlessConfig(
+      `verbose: true
+      useStderr: true`,
     );
 
     fs.writeFileSync(
@@ -279,28 +241,48 @@ describe('jest configuration', () => {
 
     return serverless
       .init()
-      .then(() => {
-        expect(serverless).toHaveProperty('service.custom.jest.verbose', true);
-        expect(serverless).toHaveProperty('service.custom.jest.useStderr', true);
-
-        return serverless.run();
-      })
+      .then(() => serverless.run())
       .catch(err => expect(err).toBeUndefined())
       .then((...serverlessResults) => {
         const [{ results }] = serverlessResults;
-        expect(jestConfig.readConfig).toHaveBeenCalled();
-
-        const [[globalConfig]] = jestConfig.readConfig.mock.calls;
-
-        expect(globalConfig).toBeDefined();
-        expect(globalConfig).toMatchObject({
-          verbose: true,
-          useStderr: true,
-        });
 
         expect(results).toBeDefined();
         expect(results).toHaveProperty('numTotalTestSuites');
         expect(results.numTotalTestSuites).toBe(2);
+      });
+  }, 10000);
+
+  it('will ignore other test files by default', () => {
+    writeServerlessConfig(
+      `verbose: true
+      useStderr: true`,
+    );
+
+    fs.writeFileSync(
+      '__tests__/utils.test.js',
+      `
+      describe('random test', () => {
+        it('runs the test', () => {
+          expect(true).toBeTruthy();
+        });
+      });
+      `,
+    );
+
+    const serverless = new Serverless({ interactive: false });
+
+    expect.hasAssertions();
+
+    return serverless
+      .init()
+      .then(() => serverless.run())
+      .catch(err => expect(err).toBeUndefined())
+      .then((...serverlessResults) => {
+        const [{ results }] = serverlessResults;
+
+        expect(results).toBeDefined();
+        expect(results).toHaveProperty('numTotalTestSuites');
+        expect(results.numTotalTestSuites).toBe(1);
       });
   }, 10000);
 });
